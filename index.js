503cd241aa13df0850a75ebba8c536c2555a8b72
dutzi <dutzi.b@gmail.com>
2016-07-02 20:10:54 +0300

    made code look nice

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 var system = require('system');
 var page = require('webpage').create();
-var credentials = require('./credentials.js');
-
-console.log('Hi', credentials.username);
 
 var URL = 'https://www.bankhapoalim.co.il/';
 var TRANSACTIONS_URL = 'https://login.bankhapoalim.co.il/ServerServices/' +
 	'plastic-cards/transactions?accountId=12-170-41654&type=current';
+
+var _credentials;
 
 function getUsername() {
 	system.stdout.write('Please enter username:');
@@ -22,90 +21,117 @@ function getPassword() {
 	return password;
 }
 
-console.log('got it', password);
+function getCredentials() {
+	var credentials = require('./credentials.js');
 
-page.open(URL, function (status) {
-	//Page is loaded!
+	if (!credentials) {
+		var username = getUsername();
+		var password = getPassword();
+
+		credentials = {
+			username: username,
+			password: password,
+		};
+	}
+
+	return credentials;
+}
+
+function focusOnUserId() {
+	page.switchToFrame(0);
+	page.evaluate(function () {
+		document.querySelector('#userID').focus();
+	});
+	page.switchToMainFrame();
+}
+
+function typeCredentials(credentials) {
+	page.sendEvent('keypress', credentials.username);
+	page.sendEvent('keypress', page.event.key.Tab);
+	page.sendEvent('keypress', credentials.password);
+}
+
+function submitForm() {
+	page.switchToFrame(0);
+	page.evaluate(function () {
+		document.querySelector('#inputSend').click();
+	});
+	page.switchToMainFrame();
+}
+
+function fetchTransactions() {
+	console.log('Fetching transactions...');
+	page.evaluate(function (url) {
+		jQuery.getJSON(url, function (data) {
+			window.__bnk_transactions = data;
+		});
+	}, TRANSACTIONS_URL);
+}
+
+function getTransactionsFromPage() {
+	return page.evaluate(function () {
+		return window.__bnk_transactions;
+	});
+}
+
+function startTransactionsInterval(callback) {
+	var interval = setInterval(function () {
+		var transactions = getTransactionsFromPage();
+		if (transactions) {
+			clearInterval(interval);
+			callback(transactions);
+		}
+	}, 200);
+}
+
+function onFirstPageOpened(status) {
 	if (status === 'success') {
 
-		page.switchToFrame(0);
-		page.evaluate(function () {
-			document.querySelector('#userID').focus();
-		});
-		page.switchToMainFrame();
-
-		// page.sendEvent('click', 1020, 428);
-		page.sendEvent('keypress', credentials.username);
-		page.sendEvent('keypress', page.event.key.Tab);
-		page.sendEvent('keypress', credentials.password);
-		// page.sendEvent('keypress', page.event.key.Tab);
-		// page.sendEvent('keypress', page.event.key.Enter);
-
-		page.switchToFrame(0);
-		page.evaluate(function () {
-			document.querySelector('#inputSend').click();
-		});
-		page.switchToMainFrame();
-
-		function fetchTransactions() {
-			console.log('Fetching transactions...');
-			page.evaluate(function (url) {
-				jQuery.getJSON(url, function (data) {
-					window.__bnk_transactions = data;
-				});
-			}, TRANSACTIONS_URL);
-		}
-
-		function getTransactionsFromPage() {
-			return page.evaluate(function () {
-				return window.__bnk_transactions;
-			});
-		}
-
-		function startTransactionsInterval() {
-			var interval = setInterval(function () {
-				var transactions = getTransactionsFromPage();
-				if (transactions) {
-					clearInterval(interval);
-					console.log('Got the transactions, here they are:');
-					console.log(JSON.stringify(transactions));
-				}
-			}, 1000);
-		}
-
-		page.onLoadFinished = function(status) {
-			console.log('Finished loading second page (status: "' + status + '")');
-			fetchTransactions();
-		};
-		page.onLoadStarted = function() {
-			console.log('Logged in, loading second page...');
-		};
-
-		page.onResourceReceived = function(response) {
-			if (response.url === TRANSACTIONS_URL) {
-				console.log('Loaded transactions');
-				startTransactionsInterval();
-			}
-		};
-
-		// page.sendEvent('keypress', page.event.key.A, null, null, 0x04000000);
-
-		// var title = page.evaluate(function() {
-		// 	return document.querySelector('title').innerText;
-		// });
-		// console.log('title', title);
-
-		// // Enter credentials
-		// page.evaluate(function () {
-
-		// });
-		console.log('Waiting for first page to load...');
-		setTimeout(function () {
-			page.render('example.png');
-			phantom.exit();
-			console.log('Done!');
-		}, 10000);
+		focusOnUserId();
+		typeCredentials(_credentials);
+		submitForm();
 	} else {
 		console.log('Error loading page');
 	}
-});
+
+}
+
+function onGotTransactions(transactions) {
+	console.log('Got the transactions, here they are:');
+	console.log(JSON.stringify(transactions));
+}
+
+// Entry point //
+
+(function main() {
+	_credentials = getCredentials()
+	console.log('Hi', _credentials.username);
+	console.log('Waiting for first page to load...');
+	page.open(URL, onFirstPageOpened)
+}());
+
+page.onLoadFinished = function(status) {
+	if (page.url.indexOf('https://login.bankhapoalim.co.il/') === 0) {
+		console.log('Finished loading second page (status: "' + status + '")');
+		fetchTransactions();
+	}
+};
+
+page.onLoadStarted = function() {
+	if (page.url) {
+		console.log('Logged in, loading second page...');
+	}
+};
+
+page.onResourceReceived = function(response) {
+	if (response.url === TRANSACTIONS_URL && response.stage === 'end') {
+		console.log('Loaded transactions');
+		startTransactionsInterval(onGotTransactions);
+	}
+};
+
+setTimeout(function () {
+	page.render('example.png');
+	phantom.exit();
+	console.log('Done!');
+}, 10000);
